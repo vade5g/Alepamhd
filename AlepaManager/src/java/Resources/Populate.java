3fb4715df9ba5358ae7a2adfe25740315a338dec
Markus <Markus@BIRDD.wlan.metropolia.fi>
2017-02-23 13:06:32 +0200

    Added REST structure and USER.JAVA, missing NOTE

--- AlepaManager/src/java/Resources/Populate.java
+++ AlepaManager/src/java/Resources/Populate.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Model;
+package Resources;
 
 /**
  *
  * @author Markus
  */
-public class Note {
+public class Populate {
     
 }
